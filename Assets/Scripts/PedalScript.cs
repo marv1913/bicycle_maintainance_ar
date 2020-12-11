68258c638184e5673d659d18f89bc3eb51a8316a
Marvin <marvin.rausch@live.de>
2020-12-11 16:46:39 +0100

    example of world spaced canvas added

--- Assets/Scripts/PedalScript.cs
+++ Assets/Scripts/PedalScript.cs
@@ -1,4 +1,4 @@
-﻿
+﻿using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,9 +9,17 @@ public class PedalScript : MonoBehaviour
     public Dropdown myDropdown;
     public Text scrollbarText;
     public bool leftPedal;
-    
+    public Transform canvasPosition;
+    public GameObject canvas;
+    public float speed = 0.1f;
+    public Vector3 canvasPostion;
+    public Vector3 canvasRotation;
+
     private Quaternion _defaultRotationWrench;
     private Quaternion _defaultRotationAllen;
+    private Vector3 _lastCanvasPosition;
+    
+   
 
     // Start is called before the first frame update
     private void Start()
@@ -19,11 +27,13 @@ public class PedalScript : MonoBehaviour
         _defaultRotationWrench = wrench.transform.localRotation;
         _defaultRotationAllen = allen.transform.localRotation;
 
-        
-        myDropdown.onValueChanged.AddListener(delegate {
-            MyDropdownValueChangedHandler(myDropdown);
-        });
+
+        myDropdown.onValueChanged.AddListener(delegate { MyDropdownValueChangedHandler(myDropdown); });
         SetScrollbarText(myDropdown.value);
+        canvas.transform.position = canvasPosition.position;
+        _lastCanvasPosition = transform.position;
+
+        InvokeRepeating("PlaceCanvas", 1f, 0.01f);
     }
 
     // Update is called once per frame
@@ -48,37 +58,37 @@ public class PedalScript : MonoBehaviour
         {
             allen.SetActive(false);
         }
-        
+        // PlaceCanvas();
+
     }
 
     private void RotateGameObject(GameObject gameObj, Vector3 rotationVector)
     {
         Quaternion currentRotation = gameObj.transform.localRotation;
-      
+
         currentRotation = Quaternion.Euler(currentRotation.eulerAngles + rotationVector);
         gameObj.transform.localRotation = currentRotation;
     }
-    
-        
+
 
     private void StartWrenchAnimation()
     {
-        
         Quaternion currentRotation = wrench.transform.localRotation;
         if (leftPedal)
         {
             currentRotation = Quaternion.Euler(currentRotation.eulerAngles + new Vector3(0, 0, -60) * Time.deltaTime);
-        
         }
         else
         {
             currentRotation = Quaternion.Euler(currentRotation.eulerAngles + new Vector3(0, 0, 60) * Time.deltaTime);
         }
+
         wrench.transform.localRotation = currentRotation;
         if (wrench.transform.localRotation.eulerAngles.z < 110 && leftPedal)
         {
             wrench.transform.localRotation = _defaultRotationWrench;
-        }else if (wrench.transform.localRotation.eulerAngles.z > 250 && !leftPedal)
+        }
+        else if (wrench.transform.localRotation.eulerAngles.z > 250 && !leftPedal)
         {
             wrench.transform.localRotation = _defaultRotationWrench;
         }
@@ -86,24 +96,44 @@ public class PedalScript : MonoBehaviour
 
     private void StartAllenAnimation()
     {
-        RotateGameObject(allen, new Vector3(0, 0, 60)*Time.deltaTime);
+        RotateGameObject(allen, new Vector3(0, 0, 60) * Time.deltaTime);
         if (allen.transform.rotation.eulerAngles.z > 100)
         {
             allen.transform.localRotation = _defaultRotationAllen;
         }
+    }
+
+    void PlaceCanvas()
+    {
+        
+        Vector3 destination = _lastCanvasPosition - transform.position; 
+        _lastCanvasPosition = transform.position;
+        
+        if (Mathf.Abs(destination.x) > 0.0005f)
+        {
+            
+            canvas.transform.position =
+                Vector3.MoveTowards(_lastCanvasPosition, transform.position, speed * Time.deltaTime) + canvasPostion;
+            canvas.transform.rotation = transform.rotation * Quaternion.Euler(canvasRotation);
+        }
+        // Debug.Log(destination.x);
+        
 
     }
-    
-    void Destroy() {
+
+    void Destroy()
+    {
         myDropdown.onValueChanged.RemoveAllListeners();
     }
- 
-    private void MyDropdownValueChangedHandler(Dropdown target) {
+
+    private void MyDropdownValueChangedHandler(Dropdown target)
+    {
         SetScrollbarText(target.value);
         Debug.Log(target.itemText.text);
     }
- 
-    public void SetDropdownIndex(int index) {
+
+    public void SetDropdownIndex(int index)
+    {
         myDropdown.value = index;
     }
 
