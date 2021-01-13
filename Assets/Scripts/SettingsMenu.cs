ef0b67959ecee48447b018c6a8ac09cc283a01c8
Marvin Rausch <marvin.rausch@live.de>
2021-01-13 14:23:54 +0100

    main menu, pause menu and first texts for switchgear component

---
+++ Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public static float vibrationDuration;
+
+    private Slider _slider;
+
+    private void Start()
+    {
+        _slider = gameObject.GetComponentInChildren<Slider>();
+        vibrationDuration = _slider.value;
+    }
+
+    public void SetDuration(float duration)
+    {
+        // set duration of vibration when target is recognized
+        vibrationDuration = duration;
+    }
+  
+}
