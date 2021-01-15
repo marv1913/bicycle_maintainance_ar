f02181b2d3ba73b468bcda152afc971099880e3f
KBuklewski <45571879+KBuklewski@users.noreply.github.com>
2021-01-15 15:05:04 +0100

    ...

--- Assets/Scripts/LookAtScript.cs
+++ Assets/Scripts/LookAtScript.cs
@@ -13,31 +13,13 @@ public class LookAtScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
         transform.LookAt(transform.position + m_Camera.transform.rotation * Vector3.forward,
             m_Camera.transform.rotation * Vector3.up);
-        
+
         var distance = Vector3.Distance(m_Camera.transform.position, transform.position);
-        gameObject.GetComponentInChildren<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, distance/5);
-        gameObject.GetComponentInChildren<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, distance/5);
+        gameObject.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, distance/3);
+        gameObject.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, distance/3);
         
-        var textsize = gameObject.GetComponentInChildren<TextMeshPro>().fontScale;
-
-        textsize = textsize + distance;
-        //ScaleCanvas();
-        
-    }
-
-    void ScaleCanvas()
-    {
-        var scale = gameObject.GetComponent<RectTransform>().localScale;
-        var distance = Vector3.Distance(m_Camera.transform.position, transform.position);
-
-        scale = scale * distance*100;
-
-        var textsize = gameObject.GetComponentInChildren<TextMeshPro>().fontSize;
-        Debug.Log("Textsize :" + textsize);
-        Debug.Log("Distance :" + distance);
-        textsize = textsize + distance;
     }
 }
---
+++ Assets/SizeScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SizeScript : MonoBehaviour
+{
+    public Camera m_Camera;
+
+    // Update is called once per frame
+    void Update()
+    {
+        var distance = Vector3.Distance(m_Camera.transform.position, transform.position);
+        //gameObject.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, distance / 5);
+        //gameObject.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, distance / 5);
+
+        RectTransform rt= gameObject.GetComponent<RectTransform>();
+        rt.sizeDelta = new Vector2(rt.sizeDelta.x * distance/10, rt.sizeDelta.y * distance/10);
+    }
+}
