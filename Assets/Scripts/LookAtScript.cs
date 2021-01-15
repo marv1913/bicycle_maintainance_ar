28df6995c78010ffb0f2bfea15525558f4f6da7a
KBuklewski <45571879+KBuklewski@users.noreply.github.com>
2021-01-15 14:46:16 +0100

    first things done

--- Assets/Scripts/LookAtScript.cs
+++ Assets/Scripts/LookAtScript.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LookAtScript : MonoBehaviour
 {
@@ -9,10 +11,33 @@ public class LookAtScript : MonoBehaviour
     
 
     // Update is called once per frame
-    void LateUpdate()
+    void Update()
     {
         
         transform.LookAt(transform.position + m_Camera.transform.rotation * Vector3.forward,
             m_Camera.transform.rotation * Vector3.up);
+        
+        var distance = Vector3.Distance(m_Camera.transform.position, transform.position);
+        gameObject.GetComponentInChildren<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, distance/5);
+        gameObject.GetComponentInChildren<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, distance/5);
+        
+        var textsize = gameObject.GetComponentInChildren<TextMeshPro>().fontScale;
+
+        textsize = textsize + distance;
+        //ScaleCanvas();
+        
+    }
+
+    void ScaleCanvas()
+    {
+        var scale = gameObject.GetComponent<RectTransform>().localScale;
+        var distance = Vector3.Distance(m_Camera.transform.position, transform.position);
+
+        scale = scale * distance*100;
+
+        var textsize = gameObject.GetComponentInChildren<TextMeshPro>().fontSize;
+        Debug.Log("Textsize :" + textsize);
+        Debug.Log("Distance :" + distance);
+        textsize = textsize + distance;
     }
 }
