6c2e157ae3e086177ab49d9ed8fdcf0da20691f5
Marvin Rausch <marvin.rausch@live.de>
2021-01-19 11:01:50 +0100

    adjusted auto sizing configuration for world space canvas

---
+++ Assets/Scripts/SizeScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+/**
+* @author Karl Buklewski
+*/
+public class SizeScript : MonoBehaviour
+{
+    public Camera m_Camera;
+
+    private RectTransform rt;
+
+    void Start()
+    {
+        rt = gameObject.GetComponent<RectTransform>();
+    }
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        var distance = Vector3.Distance(m_Camera.transform.position, transform.position);
+        rt.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, distance / 6);
+        rt.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, distance / 6);
+        
+    }
+}
