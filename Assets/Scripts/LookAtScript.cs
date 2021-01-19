fe0383693b75898cac40f5fb0274c238184f3d98
Marvin Rausch <marvin.rausch@live.de>
2021-01-19 10:03:01 +0100

    new scripts for auto sizing

--- Assets/Scripts/LookAtScript.cs
+++ Assets/Scripts/LookAtScript.cs
@@ -1,18 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LookAtScript : MonoBehaviour
 {
 
     public Camera m_Camera;
-    
 
-    // Update is called once per frame
-    void LateUpdate()
+    private RectTransform rt;
+
+    void Start()
     {
-        
+        rt = gameObject.GetComponent<RectTransform>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
         transform.LookAt(transform.position + m_Camera.transform.rotation * Vector3.forward,
             m_Camera.transform.rotation * Vector3.up);
+        
+        var distance = Vector3.Distance(m_Camera.transform.position, transform.position);
+        //Resize the Canvas if the distance changes
+        rt.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, distance / 5);
+        rt.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, distance / 5);
+        
+        
     }
 }
